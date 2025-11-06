7ba84f2e3cbfbd97fe86f84cc59a66285175e3c8
Nora <noradezabalag@gmail.com>
2025-11-06 03:16:49 +0100

    entradas.html responsive

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ console.log(burger);
 let menu_opt = document.querySelector(".menu");
 console.log(menu_opt);
 
-//Definición del evento:
 burger.addEventListener("click", function () {
   console.log("hola");
 
@@ -18,13 +17,13 @@ burger.addEventListener("click", function () {
 window.addEventListener("scroll", function () {
   const nav = document.querySelector("nav");
 
-  if (window.scrollY > 10 && window.innerWidth > 768) {
+  if (window.scrollY > 10 && window.innerWidth) {
     nav.classList.add("nav-scrolled");
   } else {
     nav.classList.remove("nav-scrolled");
   }
 });
-// Lógica del formulario de suscripción al newsletter
+// Formulario de suscripción al newsletter
 
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector(".newsletter_formulario");
